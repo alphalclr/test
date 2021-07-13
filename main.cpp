e0a220a3076ed59f9afa2ca0a7404c60b889eb10
luochuan <chuan.luo@deptrum.com>
2021-07-13 17:02:42 +0800

    [format] clang-format-10

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <stdio.h>
-#include <vector>
 #include <string>
+#include <vector>
 
-int main()
-{
-	std::vector<std::string> v_s;
+int main() {
+  std::vector<std::string> v_s;
 
-	v_s.push_back("alan");
-	v_s.push_back("thomas");
-	v_s.push_back("james");
+  v_s.push_back("alan");
+  v_s.push_back("thomas");
+  v_s.push_back("james");
 
-	for (auto &e : v_s) {
-		printf("%s\n", e.c_str());
-	}
+  for (auto &e : v_s) {
+    printf("%s\n", e.c_str());
+  }
 
-	return 0;
+  return 0;
 }
