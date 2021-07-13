f0c1b2ef2dd739bcbc12e4984a703d552510ea3c
luochuan <chuan.luo@deptrum.com>
2021-07-13 16:22:12 +0800

    [vector] add a vector

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <vector>
+#include <string>
 
 int main()
 {
-	printf("hello github\n");
+	std::vector<std::string> v_s;
+
+	v_s.push_back("alan");
+	v_s.push_back("thomas");
+	v_s.push_back("james");
+
+	for (auto &e : v_s) {
+		printf("%s\n", e.c_str());
+	}
 
 	return 0;
 }
